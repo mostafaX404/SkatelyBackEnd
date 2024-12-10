1a3eef633ef21f7175a8f377c7d01f2b9af35995
mostafaX404 <mostafagamal1770@gmail.com>
2024-12-10 18:03:42 +0200

    Add project files.

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,83 @@
+﻿using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _storeContext;
+
+        public ProductsController(StoreContext storeContext)
+        {
+            this._storeContext = storeContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+           return await _storeContext.Products.ToListAsync(); 
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await _storeContext.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<Product>> CreateProduct([FromBody] Product product)
+        {
+            _storeContext.Products.Add(product);
+
+            await _storeContext.SaveChangesAsync(); 
+
+            return product;
+        }
+
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> UpdateProduct(int id, Product product)
+        {
+            if (product.Id != id || !ProductExists(id)) 
+            { return BadRequest("Cannot update this product"); }
+
+            _storeContext.Entry(product).State = EntityState.Modified;
+
+            await _storeContext.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> DeleteProduct(int id)
+        {
+            var product = await _storeContext.Products.FindAsync();
+
+            if (product == null)
+            { return NotFound(); }
+
+            _storeContext.Products.Remove(product);
+
+            await _storeContext.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ProductExists(int id)
+        {
+            return _storeContext.Products.Any(p => p.Id == id);
+        }
+    }
+}
---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,34 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class StoreContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<Product> Products { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+           
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-RDVKRO2\\SQLEXPRESS;Database=SkinetDatabase;Trusted_Connection=SSPI;Encrypt=false;TrustServerCertificate=true");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+
+    
+
+}
